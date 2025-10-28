c188675206037a580082c433699e846df3faa91b
Lucas Forsythe <lucasforsythe2004@gmail.com>
2025-10-27 21:48:34 -0400

    Horizontal Player Movement
    Player can now move left and right.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,23 +3,29 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    [SerializeField] float moveSpeed = 10f;
     Vector2 moveInput;
+    Rigidbody2D myRigidBody;
     
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
 
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, myRigidBody.linearVelocityY);
+        myRigidBody.linearVelocity = playerVelocity;
     }
 }
