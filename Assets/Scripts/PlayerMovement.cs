11e607c28bd99c00517f7165ba6d51f1968cd87c
Lucas Forsythe <lucasforsythe2004@gmail.com>
2025-10-27 22:01:26 -0400

    Improvements
    I changed the collider to a square to improve my collisions. I adjusted player moveSpeed to 12, and I changed the camera size to 7.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float moveSpeed = 12f;
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
     
