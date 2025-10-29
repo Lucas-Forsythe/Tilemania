f9bff7c509001668e76882c8894dbcf60586c07a
Lucas Forsythe <lucasforsythe2004@gmail.com>
2025-10-29 12:26:35 -0400

    Jumping
    Added the ability to jump and alter jumping through gravity and jump speed in player movement script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 12f;
+    [SerializeField] float jumpSpeed = 5f;
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
     Animator myAnimator;
@@ -25,6 +26,15 @@ public class PlayerMovement : MonoBehaviour
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            myRigidBody.linearVelocity += new Vector2(0f, jumpSpeed);
+        }
 
     }
 
