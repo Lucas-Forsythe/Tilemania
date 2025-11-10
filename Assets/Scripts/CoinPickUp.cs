40c3b920ff4b5200f529b292867ea06e8d2c474e
Lucas Forsythe <lucasforsythe2004@gmail.com>
2025-11-09 21:01:14 -0500

    Add A Sound Effect
    Coin makes noise when picked up

--- Assets/Scripts/CoinPickUp.cs
+++ Assets/Scripts/CoinPickUp.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 
 public class CoinPickUp : MonoBehaviour
 {
+
+    [SerializeField] AudioClip coinPickupSFX;
+
+    bool coinPickup = false;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Destroy(gameObject);
+            bool coinPickup = true;
+            if (coinPickup)
+            {
+                AudioSource.PlayClipAtPoint(coinPickupSFX, transform.position);
+                Destroy(gameObject);
+            }
         }
     }
 
