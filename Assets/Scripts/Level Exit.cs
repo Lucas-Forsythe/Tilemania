8773a886a7721d9baafd6493087edb3d4441ecfe
Lucas Forsythe <lucasforsythe2004@gmail.com>
2025-11-09 21:17:54 -0500

    Make some levels and level loading
    Levels made and added the ability to move between stages.

---
+++ Assets/Scripts/Level Exit.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
