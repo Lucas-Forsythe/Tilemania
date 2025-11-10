cf681845dc0cf8eb1958eedb2b886eaa92131191
Lucas Forsythe <lucasforsythe2004@gmail.com>
2025-11-09 20:55:24 -0500

    Create Coin Pickup
    Coins exist with animation and are destroyed on pickup

---
+++ Assets/Scripts/CoinPickUp.cs
@@ -0,0 +1,14 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CoinPickUp : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
